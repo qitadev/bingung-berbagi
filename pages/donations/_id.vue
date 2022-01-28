ba06a60c03b8e38a76bca569f5d924cb4d3574c0
Albarra Naufala <albarraerdanto@gmail.com>
2022-01-28 14:05:42 +0700

    change gap in donation detail

--- pages/donations/_id.vue
+++ pages/donations/_id.vue
@@ -7,9 +7,9 @@
       >
       Donasi
     </nav>
-    <div v-if="donation" class="mt-8 flex">
-      <div class="basis-1/2 pr-16">
-        <img :src="donation.batch_img" alt="Foto Batch" class="w-full rounded border">
+    <div v-if="donation" class="mt-8 grid md:grid-cols-2 gap-8">
+      <div>
+        <img :src="donation.batch_img" alt="Foto Batch" class="w-full rounded-lg border">
         <p class="inline-block mt-8 border-b-2 border-orange-primary">
           Detail
         </p>
@@ -20,13 +20,13 @@
           Halo orang-orang baik dimanapun berada, akhir bulan ini #KitaBingungKitaBerbagi membuka donasi kembali nih untuk orang-orang yang jalanan di sekitar Yogyakarta. Donasi yang akan terkumpul akan dialokasikan untuk kebutuhan pokok, seperti makanan, minuman dan lain-lain bagi orang-orang yang membutuhkan. Langkah kecil ini harapannya bisa membantu dan mendukung satu sama lain terlebih dalam kondisi pandemi saat ini. Mengutip dari Global Hunger Index (GHI) bahwa tingkat kepalaran di Indonesia menempati urutan ketiga tertinggi di Asia Tenggara pada 2021. Indonesia mendapatkan skor indeks sebesar 18 poin atau termasuk dalam level moderat. #KitaBingungKitaBerbagi lewat donasi ini bersinergi untuk saling membantu bersama orang-orang baik semua.
         </p>
       </div>
-      <div class="basis-1/2">
+      <div>
         <h2 class="text-lg font-bold">
           Donasi Akan Ditutup Dalam
         </h2>
         <div class="mt-4 grid grid-cols-4 gap-4">
           <div>
-            <div class="text-5xl rounded bg-white border p-4 text-center">
+            <div class="text-4xl rounded-lg bg-white border p-4 text-center">
               06
             </div>
             <p class="text-center mt-2">
@@ -34,7 +34,7 @@
             </p>
           </div>
           <div>
-            <div class="text-5xl rounded bg-white border p-4 text-center">
+            <div class="text-4xl rounded-lg bg-white border p-4 text-center">
               07
             </div>
             <p class="text-center mt-2">
@@ -42,7 +42,7 @@
             </p>
           </div>
           <div>
-            <div class="text-5xl rounded bg-white border p-4 text-center">
+            <div class="text-4xl rounded-lg bg-white border p-4 text-center">
               10
             </div>
             <p class="text-center mt-2">
@@ -50,7 +50,7 @@
             </p>
           </div>
           <div>
-            <div class="text-5xl rounded bg-white border p-4 text-center">
+            <div class="text-4xl rounded-lg bg-white border p-4 text-center">
               59
             </div>
             <p class="text-center mt-2">
@@ -58,7 +58,7 @@
             </p>
           </div>
         </div>
-        <div class="mt-16 rounded border bg-white p-6">
+        <div class="mt-8 rounded-lg border bg-white p-6">
           <h2 class="text-lg mb-2">
             Bingung Berbagi Batch #8
           </h2>
@@ -77,9 +77,9 @@
                    Bank Mandiri a.n. Janneta Filza A
                  </span>
                </div>
-               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded flex justify-between items-center">
+               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded-lg flex justify-between items-center">
                  1370019442942
-                 <button class="ml-2 text-xs px-2 py-1 rounded border border-orange-primary text-orange-primary">
+                 <button class="ml-2 text-xs px-2 py-1 rounded-lg border border-orange-primary text-orange-primary">
                    Salin
                  </button>
                </div>
@@ -91,9 +91,9 @@
                    Gopay a.n. Janneta Filza A
                  </span>
                </div>
-               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded flex justify-between items-center">
+               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded-lg flex justify-between items-center">
                  087833735341
-                 <button class="ml-2 text-xs px-2 py-1 rounded border border-orange-primary text-orange-primary">
+                 <button class="ml-2 text-xs px-2 py-1 rounded-lg border border-orange-primary text-orange-primary">
                    Salin
                  </button>
                </div>
@@ -105,16 +105,16 @@
                    Shopeepay a.n. Janneta Filza A
                  </span>
                </div>
-               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded flex justify-between items-center">
+               <div class="mt-2 border bg-primary-bg-color px-4 py-2 rounded-lg flex justify-between items-center">
                  085740993396
-                 <button class="ml-2 text-xs px-2 py-1 rounded border border-orange-primary text-orange-primary">
+                 <button class="ml-2 text-xs px-2 py-1 rounded-lg border border-orange-primary text-orange-primary">
                    Salin
                  </button>
                </div>
             </li>
           </ul>
           <hr class="my-8">
-          <blockquote class="rounded border bg-primary-bg-color p-4 flex">
+          <blockquote class="rounded-lg border bg-primary-bg-color p-4 flex">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
             </svg>
