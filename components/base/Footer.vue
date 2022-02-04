49bd91c60e7518954b82231d3248a624a74e2a1c
Albarra Naufala <albarraerdanto@gmail.com>
2022-02-04 11:37:05 +0700

    change footer

--- components/base/Footer.vue
+++ components/base/Footer.vue
@@ -1,57 +1,59 @@
 <template>
   <footer id="footer" class="w-full flex flex-col">
-    <article class="w-full bg-green-primary text-white">
-      <section class="w-full sm:max-w-5xl mx-auto flex flex-col justify-between sm:flex-row py-8 px-4">
-        <section id="deskripsi" class="w-full sm:w-1/4 flex flex-col justify-start">
-          <img src="~/assets/images/bingber-logo.png" alt="logo bingber" class="w-16 h-16">
-          <p class="mt-4" style="font-size: 14px">
-            Bingung berbagi adalah gerakan sosial sekaligus wadah donasi untuk berbagi kepada kaum duafa
+    <div class="grid md:grid-cols-4 gap-8 bg-green-primary text-white p-5">
+      <div>
+        <div class="flex items-center">
+          <img src="~/assets/images/bingber-logo.png" alt="Logo Bingber" class="w-14 h-14">
+          <p class="font-bold">
+            Kita Bingung Kita Berbagi
           </p>
-        </section>
-        <section class="wrapper w-full sm:w-2/5 flex flex-col xs:flex-row justify-between items-start mt-6 xs:mt-0">
-          <section id="pintasan" class="w-full flex flex-col justify-start">
-            <h1 class="font-semibold" style="font-size: 16px">Pintasan</h1>
-            <nuxt-link to="/" class="mt-6" style="font-size: 14px">
-              Donasi
-            </nuxt-link>
-            <nuxt-link to="/" class="mt-2" style="font-size: 14px">
-              Galeri
-            </nuxt-link>
-            <nuxt-link to="/" class="mt-2" style="font-size: 14px">
-              Tentang Kami
-            </nuxt-link>
-          </section>
-          <section id="informasi" class="w-full flex flex-col justify-start sm:ml-4 md:ml-0">
-            <h1 class="font-semibold" style="font-size: 16px">Informasi</h1>
-            <p class="mt-6 break-words" style="font-size: 14px">
-              kitabingungberbagi@gmail.com
-            </p>
-            <p class="mt-2" style="font-size: 14px">
-              +62 878-3373-5341
-            </p>
-            <p class="mt-2" style="font-size: 14px">
-              Yogyakarta
-            </p>
-          </section>
-        </section>
-        <section id="ig-feeds" class="w-full sm:w-1/4 flex flex-col justify-start mt-6 sm:mt-0">
-          <h1 class="font-semibold" style="font-size: 16px">Instagram Feeds</h1>
-          <!--WIDGET DARI WOXO-->
+        </div>
+      </div>
+      <div>
+        <h2 class="font-bold uppercase mb-2">Pintasan</h2>
+        <ul class="text-sm">
+          <li class="mb-2">
+            <nuxt-link to="/donations" class="hover:underline">Donasi</nuxt-link>
+          </li>
+          <li class="mb-2">
+            <nuxt-link to="/gallery" class="hover:underline">Galeri</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about" class="hover:underline">Tentang Kami</nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="font-bold uppercase mb-2">Informasi</h2>
+        <ul class="text-sm">
+          <li class="mb-2">
+            <a href="mailto:kitabingungberbagi@gmail.com" class="hover:underline">kitabingungberbagi@gmail.com</a>
+          </li>
+          <li class="mb-2">
+            <a href="tel:+6287833735341" class="hover:underline">+62 878-3373-5341</a>
+          </li>
+          <li>
+            Yogyakarta, Indonesia
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="font-bold uppercase -mb-[18px]">Postingan Terbaru</h2>
+        <div class="padding-top: -20px;">
           <div
             loading="lazy"
-            data-mc-src="8110cafd-569d-44a6-9a70-db7dd00871e1#instagram"></div>
+            data-mc-src="8110cafd-569d-44a6-9a70-db7dd00871e1#instagram" />
           <script
             src="https://cdn2.woxo.tech/a.js#61f9205c420c7d0021f3b024"
-            async data-usrc>
-          </script>
-        </section>
-      </section>
-    </article>
-    <article class="w-full text-white" style="background-color: #0E7E71;">
+            async data-usrc />
+        </div>
+      </div>
+    </div>
+    <div class="w-full text-white" style="background-color: #0E7E71;">
       <p class="max-w-5xl mx-auto px-4 py-2 text-center">
         Bingung Berbagi © {{ new Date().getFullYear() }}
       </p>
-    </article>
+    </div>
   </footer>
 </template>
 
@@ -61,7 +63,3 @@ export default {
   name: "Footer"
 }
 </script>
-
-<style scoped>
-
-</style>
