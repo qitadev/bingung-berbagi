9c93df136262e9c863eef6bb84039ff7c45fc99f
Albarra Naufala <albarraerdanto@gmail.com>
2022-02-04 08:57:36 +0700

    remove Rp in warning message donation details

--- pages/donations/_id.vue
+++ pages/donations/_id.vue
@@ -179,7 +179,7 @@
               <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
             </svg>
             <p class="ml-4 flex-1 text-sm">
-              Gunakan kode unik "00{{ donation.batch }}" <b>contoh: Rp {{ formatNumber(100000 + +donation.batch) }}</b> serta deskripsi transfer <b>"Bingung Berbagi #{{ donation.batch }}"</b> agar kami mudah menemukan niat baik kamu ya!
+              Gunakan kode unik "00{{ donation.batch }}" <b>contoh: {{ formatNumber(100000 + +donation.batch) }}</b> serta deskripsi transfer <b>"Bingung Berbagi #{{ donation.batch }}"</b> agar kami mudah menemukan niat baik kamu ya!
             </p>
           </blockquote>
         </div>
